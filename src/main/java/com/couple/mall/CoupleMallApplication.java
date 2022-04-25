87cbdbc233c18c3797567537ca316db8076f8cbe
Hyeongseo Park <hyeongseo.park@cre.ma>
2022-04-25 23:46:01 +0900

    test05

--- src/main/java/com/couple/mall/CoupleMallApplication.java
+++ src/main/java/com/couple/mall/CoupleMallApplication.java
@@ -7,6 +7,8 @@ import org.springframework.context.annotation.PropertySource;
 @SpringBootApplication
 public class CoupleMallApplication {
     // test04
+
+	// test05
 	public static void main(String[] args) {
 		SpringApplication.run(CoupleMallApplication.class, args);
 	}
