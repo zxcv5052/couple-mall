bf84f2e4ed1c4380c28ac34c2191544fb7ccb484
EUNCHAE KIM <112812483+eunbbori@users.noreply.github.com>
2022-10-04 21:05:55 +0900

    ASD

--- src/main/java/com/couple/mall/CoupleMallApplication.java
+++ src/main/java/com/couple/mall/CoupleMallApplication.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.PropertySource;
 @SpringBootApplication
 public class CoupleMallApplication { // test01
 	public static void main(String[] args) { // test 02
-		SpringApplication.run(CoupleMallApplication.class, args); // test
+		SpringApplication.run(CoupleMallApplication.class, args); // test test
 	} // good test
 
 }
