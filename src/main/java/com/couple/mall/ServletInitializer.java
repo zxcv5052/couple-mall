5682d014c99d59110a9acb0cd29cf1b2f7262dae
EUNCHAE KIM <112812483+eunbbori@users.noreply.github.com>
2022-10-04 21:23:18 +0900

    sssssss

--- src/main/java/com/couple/mall/ServletInitializer.java
+++ src/main/java/com/couple/mall/ServletInitializer.java
@@ -8,6 +8,6 @@ public class ServletInitializer extends SpringBootServletInitializer {
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		return application.sources(CoupleMallApplication.class);
-	}
+	}//test3 test3
 
 }
