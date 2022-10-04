f9da4f4e674a743ad66b3905dca3078ebd927461
EUNCHAE KIM <112812483+eunbbori@users.noreply.github.com>
2022-10-04 20:45:24 +0900

    FEATURE - test

--- src/main/java/com/couple/mall/CoupleMallApplication.java
+++ src/main/java/com/couple/mall/CoupleMallApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
-public class CoupleMallApplication {
+public class CoupleMallApplication { // test01
 	public static void main(String[] args) {
 		SpringApplication.run(CoupleMallApplication.class, args);
 	}
